a06e1ff24de5c91c13189bf7e51bc2966dcfdfc0
Saptarick Mishra <saptarickmishra@gmail.com>
2025-12-28 11:16:52 +0530

    initial commit

---
+++ src/main/java/com/rickm/regex/RegexEngineApplication.java
@@ -0,0 +1,13 @@
+package com.rickm.regex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegexEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegexEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rickm/regex/RegexEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rickm.regex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegexEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
